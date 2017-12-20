41b2bd5038e12b546f87302016098f07f7a72333
胡宇瑶 <catherine.gobears.berkeley.edu>
2017-12-21 07:25:25 +0800

    Add ch05

---
+++ ch05/biggerBetterFavoriteNumber.rb
@@ -0,0 +1,5 @@
+puts 'Hey, what\'s your favorite number?'
+number = gets.chomp
+puts 'There is a bigger and better favorite number:'
+bigger = 1
+puts number.to_i + bigger
---
+++ ch05/fullNameGreeting.rb
@@ -0,0 +1,7 @@
+puts 'Hello there! What\'s your first name?'
+name1 = gets.chomp 
+puts 'What a great first name. What\'s your middle name?'
+name2 = gets.chomp
+puts name2 + '? Alright. Could you tell me your last name then?'
+name3 = gets.chomp
+puts "Awesome!" + " Nice to meet you, " + name1 + ' ' + name2 + ' ' + name3 + ' :)'
